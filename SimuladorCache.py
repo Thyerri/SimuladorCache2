5e08f9087c2001a60269edc7341a5a0aa9aa6769
Thyerri Schirmer <thyschirmer@gmail.com>
2016-05-14 12:02:02 -0300

    Initial commit

---
+++ SimuladorCache.py
@@ -0,0 +1,45 @@
+## TRABALHO I DE ARQUITETURA DE COMPUTADORES II
+## NOMES: CRISTIAN ZATT - 56484
+##        THYÉRRI SCHIRMER - 56692
+
+## SIMULADOR DE MEMÓRIA CACHE COM MAPEAMENTO DIRETO
+
+import sys
+
+##variáveis globais de parametros da memória, altere o valor das mesmas para configurar a memória como desejado
+setaTamEndereco = 32
+setaoffset = 2
+setaIndex = 4
+setaTag = 24
+
+
+##função que realiza a leitura do arquivo
+def abreArquivo():
+    arquivoTexto = open('C:/teste.txt', 'r', encoding="utf8")
+
+    return arquivoTexto
+
+def fechaArquivo(arq):
+    arq.close()
+
+#####################################################################################
+## FUNÇÃO PRINCIPAL
+#####################################################################################
+arquivoTxt = abreArquivo()
+
+for linha in arquivoTxt:
+    print(linha)
+    binLinha = bin(int(linha, 16))
+    esquerdaBits = (setaTamEndereco - len(binLinha))
+
+
+    strBinLinha = str(binLinha)
+    strBinLinha = strBinLinha.format(32)
+
+
+    print(strBinLinha)
+
+
+
+fechaArquivo(arquivoTxt)
+
